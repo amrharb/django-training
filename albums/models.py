f16d2a03cb1252069dd527ecc68c06e9e898f7ef
Amr Harb <65994648+amrharb@users.noreply.github.com>
2022-10-15 11:23:45 +0200

    update release time default

--- albums/models.py
+++ albums/models.py
@@ -4,7 +4,7 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, related_name="albums", on_delete=models.CASCADE)
     name = models.CharField(max_length=255,default="New Album")
     created_at = models.DateTimeField(auto_now_add=True)
-    release_at = models.DateTimeField(auto_now=True)
+    release_at = models.DateTimeField()
     cost = models.DecimalField(max_digits=8, decimal_places=2,  blank=True)
 
     def __str__(self):
